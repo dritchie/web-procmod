be54c89e2051dba34e2602acb85de89eb2853900
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2015-03-06 10:57:06 -0800

    Finished basic boxy spaceships, fixed issue with wireframe z-fighting.

--- model.js
+++ model.js
@@ -25,11 +25,6 @@ genGeometry = (function()
 
 	// ----------------------------------------------------------------
 
-	var dospacing = true;
-	var eps = 0;
-	if (dospacing) { eps = 0.005; }
-
-
 	function genShip(rearz)
 	{
 		return _genShip(new THREE.Geometry(), 0, rearz);
@@ -45,19 +40,18 @@ genGeometry = (function()
 		var wingprob = wi(i+1, 0.5);
 		// Gen wings?
 		var newgeo2 = flip(wingprob) ?
-							addWings(newgeo1, 0, 0.5*xlen+eps, rearz+0.5, rearz+zlen-0.5)
+							addWings(newgeo1, 0, 0.5*xlen, rearz+0.5, rearz+zlen-0.5)
 							:
 							newgeo1;
-		// // Gen fin?
-		// var finprob = 0.7;
-		// var newgeo3 = flip(finprob) ?
-		// 					addFin(newgeo2, 0, 0.5*ylen, rearz, rearz+zlen, 0.6*xlen)
-		// 					:
-		// 					newgeo2;
-		var newgeo3 = newgeo2;
+		// Gen fin?
+		var finprob = 0.7;
+		var newgeo3 = flip(finprob) ?
+							addFin(newgeo2, 0, 0.5*ylen, rearz, rearz+zlen, 0.6*xlen)
+							:
+							newgeo2;
 		// Continue generating?
 		if (flip(wi(i, 0.4)))
-			return _genShip(newgeo3, i+1, rearz+zlen+eps);
+			return _genShip(newgeo3, i+1, rearz+zlen);
 		else
 			return newgeo3;
 	}
@@ -69,10 +63,7 @@ genGeometry = (function()
 		var xlen = uniform(1, 3);
 		var ylen = uniform(.5, 1) * xlen;
 		var zlen = uniform(2, 5);
-		var bodygeo = new THREE.BoxGeometry(xlen, ylen, zlen);
-		var mat = new THREE.Matrix4();
-		mat.makeTranslation(0, 0, rearz + 0.5*zlen);
-		bodygeo.applyMatrix(mat);
+		var bodygeo = box(0, 0, rearz + 0.5*zlen, xlen, ylen, zlen);
 		return { xlen: xlen, ylen: ylen, zlen: zlen, bodygeo: bodygeo };
 	}
 
@@ -85,7 +76,7 @@ genGeometry = (function()
 		var zbase = rets.zbase;
 		var newgeo = geo.combine(rets.winggeo);
 		if (flip(wi(i, 0.6)))
-			return addWings(newgeo, i+1, xbase+xlen+eps, zbase-0.5*zlen, zbase+0.5*zlen);
+			return addWings(newgeo, i+1, xbase+xlen, zbase-0.5*zlen, zbase+0.5*zlen);
 		else
 			return newgeo;
 	}
@@ -96,15 +87,33 @@ genGeometry = (function()
 		var xlen = uniform(0.25, 2.0);
 		var ylen = uniform(0.25, 1.25);
 		var zlen = uniform(0.5, 4.0);
-		var winggeo1 = new THREE.BoxGeometry(xlen, ylen, zlen);
-		var mat = new THREE.Matrix4();
-		mat.makeTranslation(xbase+0.5*xlen, 0, zbase);
-		winggeo1.applyMatrix(mat);
-		var winggeo2 = new THREE.BoxGeometry(xlen, ylen, zlen);
-		mat.makeTranslation(-(xbase + 0.5*xlen), 0, zbase);
-		winggeo2.applyMatrix(mat);
+		var winggeo1 = box(xbase+0.5*xlen, 0, zbase, xlen, ylen, zlen);
+		var winggeo2 = box(-(xbase + 0.5*xlen), 0, zbase, xlen, ylen, zlen);
 		winggeo1.merge(winggeo2);
 		return { xlen: xlen, ylen: ylen, zlen: zlen, zbase: zbase, winggeo: winggeo1 }
+	}
+
+	function addFin(geo, i, ybase, zlo, zhi, xmax)
+	{
+		var xlen = uniform(0.5, 1.0) * xmax;
+		var ylen = uniform(0.1, 0.5);
+		var zlen = uniform(0.5, 1.0) * (zhi - zlo);
+		var zbase = 0.5*(zlo + zhi);
+		var fingeo = box(0, ybase + 0.5*ylen, zbase, xlen, ylen, zlen);
+		var newgeo = geo.combine(fingeo);
+		if (flip(wi(i, 0.2)))
+			return addFin(newgeo, i+1, ybase+ylen, zbase-0.5*zlen, zbase+0.5*zlen, xlen);
+		else
+			return newgeo;
+	}
+
+	function box(x, y, z, xlen, ylen, zlen)
+	{
+		var b = new THREE.BoxGeometry(xlen, ylen, zlen);
+		var m = new THREE.Matrix4();
+		m.makeTranslation(x, y, z);
+		b.applyMatrix(m);
+		return b;
 	}
 
 
