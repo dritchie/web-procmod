9b58726a426c02ed78cf9e66bb49d92e92579ce6
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2015-06-15 16:36:10 -0700

    new code mirror version that has non-broken line numbers + cursor blink.
    Also, made code editor retain position when hiding/unhiding. This turns out to be just what it does by default...unless jQuery animations are used. I still haven't figured out how to get position retention to work in this case (i.e. there's no callback that fires after an animation is finished--if there was, i could restore the position there).

--- js/codeeditor.js
+++ js/codeeditor.js
@@ -7,7 +7,7 @@ var CodeEditor = (function() {
 		textarea.text('// This is a comment!');
 		outerdiv.append(textarea);
 
-		var defaultWidth = 850;
+		var defaultWidth = 870;
 		var defaultHeight = 850;
 
 		var state = {};
@@ -19,31 +19,43 @@ var CodeEditor = (function() {
 			draggable: false,
 			resizable: true,
 			position: {my: 'left top', at: 'left top', of: window},
-			show: {effect: 'slide', duration: 100},
-			hide: {effect: 'slide', duration: 100},
+			// show: {effect: 'slide', duration: 100},
+			// hide: {effect: 'slide', duration: 100},
 			width: defaultWidth,
 			height: defaultHeight
 		});
 		state.dialog.parents(".ui-dialog").css("opacity", "0.85");
 
 		state.editor = CodeMirror.fromTextArea(document.getElementById('codeTextArea'), {
-			// lineNumbers: true,
+			lineNumbers: true,
 			matchBrackets: true,
 			autoCloseBrackets: true,
 			viewportMargin: Infinity,
 			lineWrapping: true
 		});
-		// // Stop keyboard events from propagating out of the editor
-		// state.editor.getWrapperElement().onkeypress = function(e) {
-		// 	e.stopImmediatePropagation();
-		// }
 
 		state.visible = true;
-		state.open = function() { state.dialog.dialog('open'); state.visible = true; }
-		state.close = function() { state.dialog.dialog('close'); state.visible = false; }
+		state.open = function() {
+			state.dialog.dialog('open');
+		};
+		state.close = function() {
+			state.dialog.dialog('close');
+		}
+		state.dialog.on('dialogbeforeclose', function() {
+			state.visible = false;
+			// // Remember where we were last scrolled to
+			// state.lastScrollState = state.editor.getScrollInfo();
+		});
+		state.dialog.on('dialogopen', function() {
+			state.visible = true;
+			// // Restore where we were last scrolled to
+			// if (state.lastScrollState) {
+			// 	state.editor.scrollTo(0, state.lastScrollState.top);
+			// }
+		});
 		state.setEventHandler = function(name, fn) {
 			state.dialog.on(name, fn);
-		}
+		};
 
 		state.loadCodeFromFile = function(filename, cb) {
 			// Load WebPPL procedural modeling code
@@ -51,10 +63,10 @@ var CodeEditor = (function() {
 			{
 				state.editor.setValue(code);
 				var lc = state.editor.lineCount();
-				state.editor.scrollIntoView(lc-1, 0);
+				state.editor.scrollIntoView(lc-1);
 				if (cb) cb();
 			});	
-		}
+		};
 
 		// Takes callback with what to do when the compilation is done
 		// Arg to callback is either the newly-compiled function, if the code
